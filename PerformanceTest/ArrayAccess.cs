2f0b84984a97bc130bd51168de54cc4a91035108
Noon-R <noon.ryou0304@gmail.com>
2024-06-04 08:31:58 +0900

    プロジェクト ファイルを追加します。

---
+++ PerformanceTest/ArrayAccess.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerformanceTest
+{
+	internal class ArrayAccess
+	{
+		private List<int> _ListData;
+		private int[] _ArrayData;
+
+		public ArrayAccess() {
+			int elementCount = 10000;
+
+			_ListData = new List<int>(elementCount);
+			_ArrayData = new int[elementCount];
+
+			for (int i = 0; i < elementCount; ++i) {
+				_ListData[i] = i;
+				_ArrayData[i] = i;
+			}
+		}
+
+
+		public void iterateListA() {
+			for (int i = 0; i < _ListData.Count; ++i) {
+				
+			}
+		}
+	}
+
+
+}
---
+++ PerformanceTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PerformanceTest
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+
+	}
+
+
+	internal class RefTest { 
+	}
+}
